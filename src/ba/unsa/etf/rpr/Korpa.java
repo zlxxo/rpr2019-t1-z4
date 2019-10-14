8280f9a7f55869afa2edf14a30bdcb8a247d435f
zlxxo <mramic2@etf.unsa.ba>
2019-10-14 18:53:35 +0200

    klase Supermarket i Korpa

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,6 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private final int maksimalanBrojArtikala = 50;
+    private Artikl[] artikli = new Artikl[50];
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private final int maksimalanBrojArtikala = 1000;
+    private Artikl[] artikli = new Artikl[1000];
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public void setArtikli(Artikl[] artikli) {
+        this.artikli = artikli;
+    }
 }
