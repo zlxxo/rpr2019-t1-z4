ebb38c34c6b5973bbc26f6981d96ce2f9b81b910
zlxxo <mramic2@etf.unsa.ba>
2019-10-14 18:47:32 +0200

    klasa Artikl getteri, setteri i konstruktor

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,4 +1,37 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
+    private String naziv;
+    private double cijena;
+    private String kod;
+
+    public Artikl(String naziv, double cijena, String kod) {
+        this.naziv = naziv;
+        this.cijena = cijena;
+        this.kod = kod;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public double getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(double cijena) {
+        this.cijena = cijena;
+    }
+
+    public String getKod() {
+        return kod;
+    }
+
+    public void setKod(String kod) {
+        this.kod = kod;
+    }
 }
